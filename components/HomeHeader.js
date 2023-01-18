c992334e49706b75772218c71de6860b1e69d025
Fquicy <florien.quicy@ynov.com>
2023-01-18 15:56:45 +0100

    1st

---
+++ components/HomeHeader.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { View, Text, Image, TextInput } from "react-native";
+
+import { COLORS, FONTS, SIZES, assets } from "../constants";
+
+const HomeHeader = ({ onSearch }) => {
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.font,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 50, height: 50, borderRadius: 50}}
+        />
+
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
+
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Heureux de vous revoir User 👋
+        </Text>
+      </View>
+
+      <View style={{ marginTop: SIZES.font }}>
+        <View
+          style={{
+            width: "100%",
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: "row",
+            alignItems: "center",
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2,
+          }}
+        >
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{ width: 10, height: 10, marginRight: SIZES.small }}
+          />
+          <TextInput
+            placeholder="Search"
+            style={{ flex: 1 }}
+            onChangeText={onSearch}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HomeHeader;

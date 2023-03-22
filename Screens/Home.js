14b6b43eb4546436504c5ae713bd00e03d30f2a6
ZraptorZ <zraptorzpvp@gmail.com>
2023-03-22 09:44:32 +0100

    pull

--- Screens/Home.js
+++ Screens/Home.js
@@ -63,38 +63,6 @@ export default class Home extends React.Component {
       setNftData(filteredData);
     }
   };
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar backgroundColor={COLORS.primary} />
-      <View style={{ flex: 1 }}>
-        <View style={{ zIndex: 0 }}>
-          <FlatList
-            data={nftData}
-            renderItem={({ item }) => <Card data={item} />}
-            keyExtractor={(item) => item.id}
-            showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
-          />
-        </View>
-        
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            zIndex: -1,
-          }}
-        >
-          <View
-            style={{ height: 300, backgroundColor: COLORS.primary }} />
-          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
 };
 
 export default Home;
